78d4ac84f79d7de3854f2d740af55d20870d501e
jason wong <jayywong92@gmail.com>
2018-06-15 16:03:45 -0700

    apparently, these notifications can have 3 digits now

--- youtube_number.user.js
+++ youtube_number.user.js
@@ -1,18 +1,20 @@
 // ==UserScript==
 // @name         youtube notification number remover
 // @namespace    youtubenotif
-// @version      1.0
+// @version      1.1
 // @description  removes the number in "(5) video title"
 // @author       jasonw
 // @match        https://www.youtube.com/*
 // @grant        none
 // ==/UserScript==
 
+const WIDTH = 5;
+
 function fixTitle(title) {
     // lazy and shitty way to make sure we dont trim trailing () cus i suck at regex
     // ex: song title (feat. artist)
     // bugs if a video title has () at the beginning ¯\_(ツ)_/¯
-    return title.substr(0, 4).replace(/ *\([^)]*\) */, "") + title.substr(4);
+    return title.substr(0, WIDTH).replace(/ *\([^)]*\) */, "") + title.substr(WIDTH);
 }
 window.addEventListener("load", () => {
     document.title = fixTitle(document.title);
