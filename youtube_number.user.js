1f354d5a1674ff5e3046d1488891cc9ce8b7b36e
jaasonw <jayywong92@gmail.com>
2018-09-29 23:37:06 -0700

    change to 10 seconds

--- youtube_number.user.js
+++ youtube_number.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         youtube notification number remover
 // @namespace    youtubenotif
-// @version      1.2
+// @version      1.3
 // @description  removes the number in "(5) video title"
 // @author       jasonw
 // @match        https://www.youtube.com/*
@@ -20,8 +20,8 @@ window.addEventListener("load", () => {
     // does not work on firefox lookin ass >:(
     // cheap setInterval fix because i cant be bothered to figure out why it works on one
     // browser but not another
-    window.setInterval(() => { document.title = fixTitle(document.title) }, 1000);
-    
+    window.setInterval(() => { document.title = fixTitle(document.title) }, 10000);
+
     document.title = fixTitle(document.title);
 
     let observer = new MutationObserver(() => {
