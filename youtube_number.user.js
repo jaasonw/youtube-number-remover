d16f8a966f4d741d9c764568c908448201132c26
jason wong <jayywong92@gmail.com>
2022-03-31 23:42:45 -0700

    even more cleanup

--- youtube_number.user.js
+++ youtube_number.user.js
@@ -1,21 +1,15 @@
 // ==UserScript==
 // @name         youtube notification number remover
 // @namespace    youtubenotif
-// @version      1.5
+// @version      1.6
 // @description  removes the number in "(5) video title"
 // @author       jasonw
 // @match        https://www.youtube.com/*
 // @grant        none
 // ==/UserScript==
-function fixTitle(title) {
-    return title.replace(/^(\(\d*\))\s*/, "");
-}
-
-document.title = fixTitle(document.title);
-
 const observer = new MutationObserver((e) => {
     if (e[0].addedNodes[0].data != e[0].removedNodes[0].data) {
-        document.title = fixTitle(document.title);
+        document.title = document.title.replace(/^(\(\d*\))\s*/, "");
     }
 });
 observer.observe(document.querySelector("title"), { childList: true, characterDataOldValue: true });
