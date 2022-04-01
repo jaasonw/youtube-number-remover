3998abf2c7124254575bd67880f2654bcddc0c6d
jason wong <jayywong92@gmail.com>
2022-03-31 23:27:20 -0700

    fix for firefox

--- youtube_number.user.js
+++ youtube_number.user.js
@@ -1,32 +1,21 @@
 // ==UserScript==
 // @name         youtube notification number remover
 // @namespace    youtubenotif
-// @version      1.3
+// @version      1.4
 // @description  removes the number in "(5) video title"
 // @author       jasonw
 // @match        https://www.youtube.com/*
 // @grant        none
 // ==/UserScript==
-
-const WIDTH = 5;
-
 function fixTitle(title) {
-    // lazy and shitty way to make sure we dont trim trailing () cus i suck at regex
-    // ex: song title (feat. artist)
-    // bugs if a video title has () at the beginning ¯\_(ツ)_/¯
-    return title.substr(0, WIDTH).replace(/ *\([^)]*\) */, "") + title.substr(WIDTH);
+    return title.replace(/^(\(\d*\))\W*/, "");
 }
-window.addEventListener("load", () => {
-    // does not work on firefox lookin ass >:(
-    // cheap setInterval fix because i cant be bothered to figure out why it works on one
-    // browser but not another
-    window.setInterval(() => { document.title = fixTitle(document.title) }, 10000);
 
-    document.title = fixTitle(document.title);
+document.title = fixTitle(document.title);
 
-    let observer = new MutationObserver(() => {
-        if (fixTitle(document.title) != document.title)
-            document.title = fixTitle(document.title);
-    });
-    observer.observe(document.querySelector("title"), { subtree: true, characterData: true, childList: true });
+const observer = new MutationObserver((e) => {
+    if (e[0].addedNodes[0].data != e[0].removedNodes[0].data) {
+        document.title = fixTitle(document.title);
+    }
 });
+observer.observe(document.querySelector("title"), { childList: true, characterDataOldValue: true });
