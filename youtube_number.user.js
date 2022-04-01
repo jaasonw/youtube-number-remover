dae1725e2b48971f8a1626d02fe007c9be6bd6e2
jason wong <jayywong92@gmail.com>
2022-03-31 23:44:51 -0700

    this is getting out of hand

--- youtube_number.user.js
+++ youtube_number.user.js
@@ -1,15 +1,14 @@
 // ==UserScript==
 // @name         youtube notification number remover
 // @namespace    youtubenotif
-// @version      1.6
+// @version      1.7
 // @description  removes the number in "(5) video title"
 // @author       jasonw
 // @match        https://www.youtube.com/*
 // @grant        none
 // ==/UserScript==
-const observer = new MutationObserver((e) => {
+new MutationObserver((e) => {
     if (e[0].addedNodes[0].data != e[0].removedNodes[0].data) {
         document.title = document.title.replace(/^(\(\d*\))\s*/, "");
     }
-});
-observer.observe(document.querySelector("title"), { childList: true, characterDataOldValue: true });
+}).observe(document.querySelector("title"), { childList: true, characterDataOldValue: true });
